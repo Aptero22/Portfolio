1f1db8ab87e54d4cf04afbdc235b73ab6b9e3b16
Aptero <staslapa22@gmail.com>
2021-01-02 20:12:57 +0700

    Portfolio_v1

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// Скрипт для бургера
+const hamburger = document.querySelector('.hamburger'),
+	  menu = document.querySelector('.menu'),
+	  closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
+
+// Скрипт для работы показателей навыков
+const counters = document.querySelectorAll('.skills__ratings-counter');
+	  lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
